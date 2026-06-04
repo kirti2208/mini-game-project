50f225cda7bd4c3ef77765c92a99ba235f9bf180
kirti rajput <kirtirajput1203@gmail.com>
2026-06-04 18:37:27 +0530

    Add files via upload

---
+++ tictactoe.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'}
+};
+
+char currentPlayer = 'X';
+
+void displayBoard() {
+    cout << "\n";
+    for(int i = 0; i < 3; i++) {
+        cout << " ";
+        for(int j = 0; j < 3; j++) {
+            cout << board[i][j];
+            if(j < 2) cout << " | ";
+        }
+        cout << "\n";
+        if(i < 2) cout << "---|---|---\n";
+    }
+    cout << "\n";
+}
+
+bool makeMove(int choice) {
+    int row = (choice - 1) / 3;
+    int col = (choice - 1) % 3;
+
+    if(board[row][col] != 'X' && board[row][col] != 'O') {
+        board[row][col] = currentPlayer;
+        return true;
+    }
+    return false;
+}
+
+bool checkWin() {
+    for(int i = 0; i < 3; i++) {
+        if(board[i][0] == currentPlayer &&
+           board[i][1] == currentPlayer &&
+           board[i][2] == currentPlayer)
+            return true;
+
+        if(board[0][i] == currentPlayer &&
+           board[1][i] == currentPlayer &&
+           board[2][i] == currentPlayer)
+            return true;
+    }
+
+    if(board[0][0] == currentPlayer &&
+       board[1][1] == currentPlayer &&
+       board[2][2] == currentPlayer)
+        return true;
+
+    if(board[0][2] == currentPlayer &&
+       board[1][1] == currentPlayer &&
+       board[2][0] == currentPlayer)
+        return true;
+
+    return false;
+}
+
+bool checkDraw() {
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(board[i][j] != 'X' && board[i][j] != 'O')
+                return false;
+        }
+    }
+    return true;
+}
+
+void resetBoard() {
+    char num = '1';
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            board[i][j] = num++;
+        }
+    }
+}
+
+int main() {
+    char replay;
+
+    do {
+        resetBoard();
+        currentPlayer = 'X';
+
+        while(true) {
+            displayBoard();
+
+            int choice;
+            cout << "Player " << currentPlayer
+                 << ", enter position (1-9): ";
+            cin >> choice;
+
+            if(choice < 1 || choice > 9 || !makeMove(choice)) {
+                cout << "Invalid move! Try again.\n";
+                continue;
+            }
+
+            if(checkWin()) {
+                displayBoard();
+                cout << "Player " << currentPlayer << " wins!\n";
+                break;
+            }
+
+            if(checkDraw()) {
+                displayBoard();
+                cout << "Game Draw!\n";
+                break;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+
+        cout << "Play Again? (Y/N): ";
+        cin >> replay;
+
+    } while(replay == 'Y' || replay == 'y');
+
+    cout << "Thanks for playing!\n";
+    return 0;
+}
